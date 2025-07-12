3c9f6c01364e77c6a01efa34b5e599e7819522b3
azmine-1 <sachchha222@gmail.com>
2025-07-12 16:11:40 -0400

    added step function for cpu

--- SAP-1/main.go
+++ SAP-1/main.go
@@ -3,19 +3,45 @@ package main
 import "fmt"
 
 type CPU struct {
-	A   uint8
-	B   uint8
-	PC  uint8
-	MAR uint8
-	IR  uint8
-	OUT uint8
+	A          uint8
+	B          uint8
+	PC         uint8
+	MAR        uint8
+	IR         uint8
+	OUT        uint8
+	Z          bool
+	clockCycle int
+	memory     [16]uint8
 }
 
-// struct I represents instructions
-// named I for convenience
-type I struct {
-	O uint8 // opcode
-	M uint8 // memory address
+type Instruction struct {
+	Opcode       uint8
+	MemoryAdress uint8
+}
+
+func splitInstruction(RI uint8) Instruction {
+	instruction := Instruction{}
+	instruction.Opcode = (RI & 0xF0) >> 4
+	instruction.MemoryAdress = RI & 0x0F
+	return instruction
+}
+
+func step(c *CPU, i *Instruction) {
+	switch c.clockCycle {
+	case 0:
+		c.MAR = c.PC
+	case 1:
+		c.PC++
+	case 2:
+		c.IR = c.memory[c.MAR]
+	case 3:
+		execute(c, i)
+	}
+
+}
+
+func execute(c *CPU, i *Instruction) {
+	return (nil)
 }
 
 func main() {
